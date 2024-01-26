b488bd9ccc5e29d2ed01faba93ad7db1224b2f05
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2024-01-25 18:01:49 -0600

    model validation exercise

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,16 +1,48 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.*;
+
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId = 1;
-
+    @NotBlank(message = "Name is required")
+    @Size(min=3, max=30, message = "Name must be in between 3 and 30 characters")
     private String name;
+
+    @Size(max=500, message = "Description too long")
     private String description;
-    public Event(String name, String description) {
+
+    @NotBlank(message = "Email is required")
+    @Email(message="Invalid email. Try again")
+    private String contactEmail;
+
+    @NotBlank(message = "Location cannot be left blank.")
+    @NotNull
+    private String location;
+
+    @NotBlank(message = "Attendees must be registered")
+    private String registered;
+
+    @NotBlank(message = "Number of attendees cannot be left blank")
+    @Min(value = 1, message = "Minimum number of attendees is 1")
+    private int numAttendees;
+    private EventType type;
+
+    public Event(String name, String description, String contactEmail, EventType type, String location, String registered, int numAttendees) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.type = type;
+        this.location = location;
+        this.registered = registered;
+        this.numAttendees = numAttendees;
+
+    }
+
+    public Event() {
         this.id=nextId;
         nextId++;
     }
@@ -33,6 +65,46 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumAttendees() {
+        return numAttendees;
+    }
+
+    public void setNumAttendees(int numAttendees) {
+        this.numAttendees = numAttendees;
+    }
+
+    public String getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(String registered) {
+        this.registered = registered;
     }
 
     @Override
